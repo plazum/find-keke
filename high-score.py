49c82647b1b9dc56c92939968bc02b2547aa3ef4
plazum <34277374+plazum@users.noreply.github.com>
2021-12-18 16:06:22 +0800

    在刷新网页之前保留用户上一次提交的玩家名

--- high-score.py
+++ high-score.py
@@ -1,5 +1,5 @@
 import re
-from math import *
+from math import ceil, log1p
 
 pattern1 = re.compile(r"^[|] (.+) [|] (\d+) [|] (\d+) [|] ([-]?\d+[.]\d\d) [|]"
                       r" (([A-Z][a-z][a-z] ){2}\d\d \d\d\d\d \d\d:\d\d:\d\d GMT[+-]\d\d\d\d [(](.+)[)]) [|]$")
@@ -9,9 +9,9 @@ pattern2 = re.compile(r"^[|] ([*][*])(.+)\1 [|] \1(\d+)\1 [|] \1(\d+)\1 [|] \1([
 
 def regex_match(line):
     if match := pattern1.match(line):
-        return [match.group(1), match.group(2), match.group(3), match.group(4), match.group(5)]
+        return tuple(match.group(i) for i in (1, 2, 3, 4, 5))
     match = pattern2.match(line)
-    return [match.group(2), match.group(3), match.group(4), match.group(5), match.group(6)]
+    return tuple(match.group(i) for i in (2, 3, 4, 5, 6))
 
 
 with open(".idea/comment.txt", "r", encoding="UTF-8") as f:
@@ -19,11 +19,11 @@ with open(".idea/comment.txt", "r", encoding="UTF-8") as f:
 lines = list(map(lambda l: l.removesuffix("\n"), lines))
 high_score_index = set()
 start = lines.index("| --- | --- | --- | --- | --- |") + 1
-record = {}
+record_dict = {}
 
 for i in range(start, len(lines)):
     match = regex_match(lines[i])
-    match.append(i)  # 用于后面的high_score_index
+    match += (i,)  # 用于后面的high_score_index
     lines[i] = match
     time = float(match[3])
     # 小于0.1秒的分数不计
@@ -31,19 +31,22 @@ for i in range(start, len(lines)):
         continue
     rows = int(match[1])
     cols = int(match[2])
-    if (rows, cols) not in record:
-        record[(rows, cols)] = [match]
+    if (rows, cols) not in record_dict:
+        record_dict[(rows, cols)] = [match]
     else:
-        record[(rows, cols)].append(match)
-# 给记录排序并保留前ceil(ln(x + 1))项
-record = [record[map_size] for map_size in sorted(record.keys(), key=lambda x: (x[0] * x[1], x[1]))]
-for i in range(len(record)):
-    record[i].sort(key=lambda x: float(x[3]))
-    record[i] = record[i][:ceil(log1p(len(record[i])))]
-    high_score_index.add(record[i][0][5])
+        record_dict[(rows, cols)].append(match)
+# 给记录排序并给每种棋盘大小保留前ceil(ln(x + 1))项
+record_list = [record_dict[map_size] for map_size in sorted(record_dict.keys(), key=lambda x: (x[0] * x[1], x[1]))]
+for i in range(len(record_list)):
+    record_list[i].sort(key=lambda x: float(x[3]))
+    record_list[i] = record_list[i][:ceil(log1p(len(record_list[i])))]
+    high_score_index.add(record_list[i][0][5])
 
 with open(".idea/processed.txt", "w", encoding="UTF-8") as f:
-    f.write("# 最高纪录（计分板" + lines[0][10] + "）\n")
+    board_num = next(l for l in lines if l.startswith("# 计分板"))[5]
+    f.write("<!--\n# 请看下方的新计分板（[点我跳转](#issuecomment-996055496)）\n")
+    f.write("本计分板已经达到长度限制，无法继续更新。感谢大家的热情支持！\n-->\n")
+    f.write("# 最高纪录（计分板" + board_num + "）\n")
     f.write("（按照棋盘大小和用时排序。）\n")
     f.write("（当前最高纪录用粗体表示。）\n")
     f.write("（对于每种棋盘大小，显示前![lagrida_latex_editor(ceil(ln(x+1))).png]"
@@ -53,8 +56,8 @@ with open(".idea/processed.txt", "w", encoding="UTF-8") as f:
     f.write("（部分分数开头的999是我怀疑其作弊而添加的用于标注的前缀。）\n")
     f.write("| 玩家名 | 行 | 列 | 用时/秒 | 时间 |\n")
     f.write("| --- | ---: | ---: | ---: | --- |\n")
-    for record_of_a_map in record:
-        line = "| **%s** | **%s** | **%s** | **%s** | **%s** |"\
+    for record_of_a_map in record_list:
+        line = "| **%s** | **%s** | **%s** | **%s** | **%s** |" \
                % (record_of_a_map[0][0], record_of_a_map[0][1], record_of_a_map[0][2], record_of_a_map[0][3],
                   record_of_a_map[0][4])
         f.write(line + "\n")
@@ -63,8 +66,8 @@ with open(".idea/processed.txt", "w", encoding="UTF-8") as f:
                    % (record_of_a_map[i][0], record_of_a_map[i][1], record_of_a_map[i][2], record_of_a_map[i][3],
                       record_of_a_map[i][4])
             f.write(line + "\n")
-    f.write("# 计分板" + lines[0][10] + "\n")
-    f.write("<!--<details><summary>展开%d条记录</summary>\n-->\n" % (len(lines) - start))
+    f.write("# 计分板" + board_num + "\n")
+    f.write("<!--\n<details><summary>展开%d条记录</summary>\n\n-->\n" % (len(lines) - start))
     f.write("| 玩家名 | 行 | 列 | 用时/秒 | 时间 |\n")
     f.write("| --- | ---: | ---: | ---: | --- |\n")
     f.write("| player name | 14 | 10 | 0.00 | Thu Jan 01 1970 08:00:00 GMT+0800 (中国标准时间) |\n")
