720394166601132930e96172c66fe9c7e5daaf8a
Plazum Z.R. I <34277374+plazum@users.noreply.github.com>
2025-05-19 15:39:53 +0800

    添加莲之空女学院学园偶像俱乐部2025年新加入角色的名字，通过注释补充所有团体的名字

--- script-in-head.js
+++ script-in-head.js
@@ -185,45 +185,78 @@ async function get_token() {
 }
 
 const placeholder = {
-    zh: [/*"高坂穗乃果", "南小鸟", "园田海未", "西木野真姬", "星空凛", "小泉花阳", "矢泽妮可", "东条希", "绚濑绘里",
+    zh: [
+        /* // μ's
+        "高坂穗乃果", "南小鸟", "园田海未", "西木野真姬", "星空凛", "小泉花阳", "矢泽妮可", "东条希", "绚濑绘里",
+        // A-RISE
         "绮罗翼", "统堂英玲奈", "优木杏树",
+        // Aqours
         "高海千歌", "渡边曜", "樱内梨子", "国木田花丸", "黑泽露比", "津岛善子", "黑泽黛雅", "松浦果南", "小原鞠莉",
+        // Saint Snow
         "鹿角圣良", "鹿角理亚",
+        // 虹咲学园学园偶像同好会
         "高咲侑",
         "上原步梦", "中须霞", "樱坂雫", "朝香果林", "宫下爱", "近江彼方", "优木雪菜", "艾玛·维尔德", "天王寺璃奈",
         "三船栞子", "米娅·泰勒", "钟岚珠",
+        // Liella!
         "涩谷香音", "唐可可", "岚千砂都", "平安名堇", "叶月恋",
         "樱小路希奈子", "米女芽衣", "若菜四季", "鬼冢夏美",
         "维恩·玛格丽特", "鬼冢冬毬",
-        "圣泽悠奈", "柊摩央",*/
+        // Sunny Passion
+        "圣泽悠奈", "柊摩央", */
+        // 莲之空女学院学园偶像俱乐部
         "日野下花帆", "村野沙耶香", "乙宗梢", "夕雾缀理", "大泽瑠璃乃", "藤岛慈",
-        "百生吟子", "徒町小铃", "安养寺姬芽"],
-    ja: [/*"高坂穂乃果", "南ことり", "園田海未", "西木野真姫", "星空凛", "小泉花陽", "矢澤にこ", "東條希", "絢瀬絵里",
+        "百生吟子", "徒町小铃", "安养寺姬芽",
+        "塞拉斯·柳田·利林费尔德", "桂城泉"
+    ],
+    ja: [
+        /* // μ's
+        "高坂穂乃果", "南ことり", "園田海未", "西木野真姫", "星空凛", "小泉花陽", "矢澤にこ", "東條希", "絢瀬絵里",
+        // A-RISE
         "綺羅ツバサ", "統堂英玲奈", "優木あんじゅ",
+        // Aqours
         "高海千歌", "渡辺曜", "桜内梨子", "国木田花丸", "黒澤ルビィ", "津島善子", "黒澤ダイヤ", "松浦果南", "小原鞠莉",
+        // Saint Snow
         "鹿角聖良", "鹿角理亞",
+        // 虹ヶ咲学園スクールアイドル同好会
         "高咲侑",
         "上原歩夢", "中須かすみ", "桜坂しずく", "朝香果林", "宮下愛", "近江彼方", "優木せつ菜", "エマ·ヴェルデ", "天王寺璃奈",
         "三船栞子", "ミア·テイラー", "鐘嵐珠",
+        // Liella!
         "澁谷かのん", "唐可可", "嵐千砂都", "平安名すみれ", "葉月恋",
         "桜小路きな子", "米女メイ", "若菜四季", "鬼塚夏美",
         "ウィーン·マルガレーテ", "鬼塚冬毬",
-        "聖澤悠奈", "柊摩央",*/
+        // Sunny Passion
+        "聖澤悠奈", "柊摩央", */
+        // 蓮ノ空女学院スクールアイドルクラブ
         "日野下花帆", "村野さやか", "乙宗梢", "夕霧綴理", "大沢瑠璃乃", "藤島慈",
-        "百生吟子", "徒町小鈴", "安養寺姫芽"],
-    en: [/*"Kousaka Honoka", "Minami Kotori", "Sonoda Umi", "Nishikino Maki", "Hoshizora Rin", "Koizumi Hanayo", "Yazawa Nico", "Toujou Nozomi", "Ayase Eri",
+        "百生吟子", "徒町小鈴", "安養寺姫芽",
+        "セラス·柳田·リリエンフェルト", "桂城泉"
+    ],
+    en: [
+        /* // μ's
+        "Kousaka Honoka", "Minami Kotori", "Sonoda Umi", "Nishikino Maki", "Hoshizora Rin", "Koizumi Hanayo", "Yazawa Nico", "Toujou Nozomi", "Ayase Eri",
+        // A-RISE
         "Kira Tsubasa", "Toudou Erena", "Yuuki Anju",
+        // Aqours
         "Takami Chika", "Watanabe You", "Sakurauchi Riko", "Kunikida Hanamaru", "Kurosawa Ruby", "Tsushima Yoshiko", "Kurosawa Dia", "Matsuura Kanan", "Ohara Mari",
+        // Saint Snow
         "Kazuno Sarah", "Kazuno Leah",
+        // Nijigasaki High School Idol Club
         "Takasaki Yuu",
         "Uehara Ayumu", "Nakasu Kasumi", "Ousaka Shizuku", "Asaka Karin", "Miyashita Ai", "Konoe Kanata", "Yuuki Setsuna", "Emma Verde", "Tennouji Rina",
         "Mifune Shioriko", "Mia Taylor", "Zhong Lanzhu",
+        // Liella!
         "Shibuya Kanon", "Tang Keke", "Arashi Chisato", "Heanna Sumire", "Hazuki Ren",
         "Sakurakouji Kinako", "Yoneme Mei", "Wakana Shiki", "Onitsuka Natsumi",
         "Wien Margarete", "Onitsuka Tomari",
-        "Hijirisawa Yuuna", "Hiiragi Mao",*/
+        // Sunny Passion
+        "Hijirisawa Yuuna", "Hiiragi Mao", */
+        // Hasunosora Girls' High School Idol Club
         "Hinoshita Kaho", "Murano Sayaka", "Otomune Kozue", "Yuugiri Tsuzuri", "Oosawa Rurino", "Fujishima Megumi",
-        "Momose Ginko", "Kachimachi Kosuzu", "Anyouji Hime"]
+        "Momose Ginko", "Kachimachi Kosuzu", "Anyouji Hime",
+        "Ceras Yanagida Lilienfeld", "Katsuragi Izumi"
+    ]
 };
 let name_default, name_safe_for_html;
 
